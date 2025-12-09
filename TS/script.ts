136a0672f89ff891b885ab154050ffd934367422
Marcos <malome2@jviladoms.cat>
2025-12-09 09:44:50 +0100

    boton log out

---
+++ TS/script.ts
@@ -0,0 +1,13 @@
+window.onload = function () {
+    const usuariActual = localStorage.getItem("usuariActual");
+    if (!usuariActual) {
+
+        window.location.href = "login.html";
+    }
+
+}
+
+function tancarSesio() {
+    localStorage.removeItem("usuariActual");
+    window.location.href = "login.html";
+}
--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,11 @@
 "use strict";
+window.onload = function () {
+    const usuariActual = localStorage.getItem("usuariActual");
+    if (!usuariActual) {
+        window.location.href = "login.html";
+    }
+};
+function tancarSesio() {
+    localStorage.removeItem("usuariActual");
+    window.location.href = "login.html";
+}
